838e9456ead037ce924185241c71a83522004700
Vitor Melo <89869031+VitorMeloDev@users.noreply.github.com>
2024-05-20 07:47:19 -0300

    Challenge - Done

--- Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
+++ Prototype 4/Assets/Challenge 4/Scripts/PlayerControllerX.cs
@@ -38,8 +38,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Powerup"))
         {
             Destroy(other.gameObject);
-            hasPowerup = true;
-            powerupIndicator.SetActive(true);
+            StartCoroutine(PowerupCooldown());
         }
     }
 
@@ -57,7 +56,7 @@ public class PlayerControllerX : MonoBehaviour
         if (other.gameObject.CompareTag("Enemy"))
         {
             Rigidbody enemyRigidbody = other.gameObject.GetComponent<Rigidbody>();
-            Vector3 awayFromPlayer =  transform.position - other.gameObject.transform.position; 
+            Vector3 awayFromPlayer = other.gameObject.transform.position - transform.position; 
            
             if (hasPowerup) // if have powerup hit enemy with powerup force
             {
